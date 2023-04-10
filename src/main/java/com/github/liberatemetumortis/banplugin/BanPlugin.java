3f03ef1fb27686536443b8d32a9f3a24528921b2
LiberaTeMetuMortis <78408599+LiberaTeMetuMortis@users.noreply.github.com>
2023-04-11 01:42:42 +0300

    style: Create getters for db and instance

--- src/main/java/com/github/liberatemetumortis/banplugin/BanPlugin.java
+++ src/main/java/com/github/liberatemetumortis/banplugin/BanPlugin.java
@@ -2,14 +2,21 @@ package com.github.liberatemetumortis.banplugin;
 import com.github.liberatemetumortis.banplugin.database.Database;
 import com.github.liberatemetumortis.banplugin.handlers.CommandHandlers;
 import com.github.liberatemetumortis.banplugin.handlers.EventHandlers;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import static com.github.liberatemetumortis.banplugin.database.ModerationRecord.fetchBanRecords;
 
 public final class BanPlugin extends JavaPlugin {
-    public static Database db;
-    public static BanPlugin instance;
+    public Database getDb() {
+        return db;
+    }
+
+    public static BanPlugin getInstance() {
+        return instance;
+    }
+
+    private Database db;
+    private static BanPlugin instance;
     @Override
     public void onEnable() {
         instance = this;
